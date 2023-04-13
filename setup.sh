a5c26a9ea530cf07b4b9d87e4358fba01e19afd5
borbigo <83850487+borbigo@users.noreply.github.com>
2023-04-13 09:21:40 -0700

    setup setup.sh and requirements.txt

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create a virtual environment
+python3 -m venv myenv
+source myenv/bin/activate
+
+# Install the required packages
+pip install -r requirements.txt
+
+# Deactivate the virtual environment
+deactivate
