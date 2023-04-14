d4b551e58e8c4fd8ffd71fddd14387835d353df0
kassafur <furqankassa2020@gmail.com>
2023-04-13 19:54:57 -0700

    Fixing

--- backend/setup.sh
+++ backend/setup.sh
@@ -9,3 +9,11 @@ pip install -r requirements.txt
 
 # Deactivate the virtual environment
 deactivate
+
+# to start react, be on the client directory
+# in the terminal
+# write the command
+npm start
+
+# to start flask
+flask run
